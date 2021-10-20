1ee83359ab60ac74970fee1ea3a624805b5c4e75
Valentin <valentin.0.gregor@gmail.com>
2021-10-20 15:54:37 +0200

    Webgl
    added graphics and a lot of fixes

---
+++ js/app.js
@@ -0,0 +1,11 @@
+new hoverEffect({
+    parent: document.querySelector('.distortion'),
+    intesity: 0.3,
+    image1: './img/janrozman1.jpg',
+    image2: './img/janrozman2.jpg',
+    speedIn: 0.8,
+    speedOut: 1,
+    angle: Math.PI / 8,
+    imagesRatio: 5 / 4,
+    displacementImage: './img/heightMap.png'
+})
